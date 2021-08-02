d7db47fa7dceaf2f5a8ac21438ad6c67c3e61141
Giorgio Andrei <cipuandrei@libero.it>
2021-08-02 19:21:20 +0200

    new-functions-added

--- script.js
+++ script.js
@@ -5,24 +5,51 @@ var root = new Vue(
         el: '#root',
         data: {
             dailyTasks: [
-                'Take a shower',
-                'Have breakfast',
-                'Brush your theet',
-                'Get dressed',
-
+                { text: 'Take a shower', done: false, },
+                { text: 'Have breakfast', done: false, },
+                { text: 'Brush your theet', done: false, },
+                { text: 'Get dressed', done: false, },
             ],
             newTask: '',
-
+            accomplishedTasksNumber: 0,
+            filteredTasks: '',
+            searchTerm: '',
+            isFilterVisible: false,
+            isTaskDone: false,
         },
         methods: {
             addNewTask() {
-                if (this.newTask.trim() !== "") {
-                    this.dailyTasks.push(this.newTask);
-                }
+                this.newTask.trim() !== "" ? this.dailyTasks.push({ text: this.newTask, done: false, }) : alert(`YOU CANNOT ADD AN EMPTY TASK TO THE LIST.
+                Please try again.`);
                 this.newTask = "";
             },
             deleteTask(i) {
-                this.dailyTasks.splice(i, 1)
+                this.dailyTasks.splice(i, 1);
+                this.accomplishedTasksNumber = this.accomplishedTasksNumber + 1;
             },
+            emptyContent() {
+                this.newTask = "";
+            },
+            showMe(text) {
+                if (!this.searchTerm.trim()) return true;
+                const filter = this.searchTerm.trim().toLowerCase();
+                text = text.toLowerCase();
+                return text.includes(filter);
+            },
+            toggleFilter() {
+                this.isFilterVisible = !this.isFilterVisible;
+                this.searchTerm = '';
+            },/* 
+            taskStatus(index) {
+                const taskStatus = this.dailyTasks.map((task, taskIndex) => {
+                    if (taskIndex === index) {
+                        task.done = !task.done;
+                    }
+                    return task;
+                });
+                this.dailyTasks = updatedTasks;
+            },
+            status */
+
         },
     });
